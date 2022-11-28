50eac924f275525c394656899ece72604ccdf84c
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-11-28 17:54:36 -0500

    Add React and first project files

---
+++ my-app/src/testimonios-fcc/components/Testimonio.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Testimonio() {
+  return (
+    <div className="contenedor-testimonio">
+      <img 
+        className="testimonio__imagen" 
+        src={require("../images/testimonio-emma.png")}
+        alt="emma photo"/>
+    </div>
+  );
+}
