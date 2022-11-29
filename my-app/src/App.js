04d8f1137c359cf9b7f7532f97227dcc6fa0847c
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-11-28 20:12:03 -0500

    Add styling to first component of first project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import Testimonio from './testimonios-fcc/components/Testimonio';
 function App() {
   return (
     <main className="App">
+      <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
       <Testimonio />
     </main>
   );
--- my-app/src/testimonios-fcc/components/Testimonio.js
+++ my-app/src/testimonios-fcc/components/Testimonio.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../styles/Testimonio.css';
 
 function Testimonio() {
   return (
